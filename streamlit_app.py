691bea616cc63eed5c032a7548744f0ec9bc394e
crzyfundude <89551936+crzyfundude@users.noreply.github.com>
2023-11-29 22:22:03 -0600

    Update streamlit_app.py
    V3 CHANGE

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My Parents New Healthy Dinner- DUDE')
+streamlit.title('My Parents New Healthy Dinner- 1')
+streamlit.title('My Parents New Healthy Dinner- 2')
+streamlit.title('My Parents New Healthy Dinner- 3')
+streamlit.title('My Parents New Healthy Dinner- 4')
