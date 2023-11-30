3d3a5ce4017ac04e554e66d1a70111ce4ea1ff04
crzyfundude <89551936+crzyfundude@users.noreply.github.com>
2023-11-29 22:47:46 -0600

    Update streamlit_pandas.py
    v7

--- streamlit_pandas.py
+++ streamlit_pandas.py
@@ -6,6 +6,9 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # lets pick the list
-streamlit.multiselect("pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_Selected = streamlit.multiselect("pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 #display the data
-streamlit.dataframe(my_fruit_list)
+#streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(fruits_to_show)
+
