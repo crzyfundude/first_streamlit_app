a87ec15e636a600abd0078c0b381b40eb1246d80
crzyfundude <89551936+crzyfundude@users.noreply.github.com>
2023-11-29 22:23:18 -0600

    Update streamlit_app.py
    V3 SMALL CHANGES FROM LESSON

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,9 @@ streamlit.title('My Parents New Healthy Dinner- 1')
 streamlit.title('My Parents New Healthy Dinner- 2')
 streamlit.title('My Parents New Healthy Dinner- 3')
 streamlit.title('My Parents New Healthy Dinner- 4')
+
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
