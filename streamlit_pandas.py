309a57b5d2e2e5d40ba7eaca3ecf9b15ed8e24e3
crzyfundude <89551936+crzyfundude@users.noreply.github.com>
2023-11-29 22:50:18 -0600

    Update streamlit_pandas.py
    v9

--- streamlit_pandas.py
+++ streamlit_pandas.py
@@ -5,6 +5,10 @@ streamlit.title('Import File From 3s Bucket - Snowflake course2:lesson3')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
+# ------------------------------------------------------------------------
+# for more information
+# https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.loc.html 
+# ------------------------------------------------------------------------
 # lets pick the list
 fruits_selected = streamlit.multiselect("pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
