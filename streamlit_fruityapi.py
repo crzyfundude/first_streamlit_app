fe0285108b7d56ff334ca1cce7e4c4267049ec2f
crzyfundude <89551936+crzyfundude@users.noreply.github.com>
2023-12-02 15:53:52 -0600

    Update streamlit_fruityapi.py

--- streamlit_fruityapi.py
+++ streamlit_fruityapi.py
@@ -4,8 +4,12 @@ import pandas
 streamlit.header('Fruityvice Fruit Advice!')
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())  #just writes data to screen
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+#streamlit.text(fruityvice_response.json())  #just writes data to screen
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
+#streamlit.text(fruityvice_response.json())  #just writes data to screen
+
 
 # take the json version of the response and normalize it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
