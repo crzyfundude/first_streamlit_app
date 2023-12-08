927dbe6af65bd87bce2b94092f3f06a36bf6d657
crzyfundude <89551936+crzyfundude@users.noreply.github.com>
2023-12-07 20:05:18 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,15 +47,15 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 
 streamlit.header('🍌🥭 FRUITY VICE  ADVICE! 🥝🍇')
-   try:
-      fruit_choice = streamlit.text_input('What fruit woud you like information about?')
-      if not fruit_choice:
-            streamlit.error("please select a fruit to get information.")
-      else:
-            fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
-            fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-            streamlit.dataframe(fruityvice_normalized)
-   except URLError as e:
+try:
+   fruit_choice = streamlit.text_input('What fruit woud you like information about?')
+   if not fruit_choice:
+          streamlit.error("please select a fruit to get information.")
+   else:
+         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
+         fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+         streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
       streamlit.error()   
 
 
